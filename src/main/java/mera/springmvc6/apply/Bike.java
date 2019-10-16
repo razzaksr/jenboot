8a08ca793508eab13f5fd8c38731655d3428bf78
razzaksr <47131600+razzaksr@users.noreply.github.com>
2019-10-16 12:49:02 +0530

    Update Bike.java

--- src/main/java/mera/springmvc6/apply/Bike.java
+++ src/main/java/mera/springmvc6/apply/Bike.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-//@Table(name="bikes")
+@Table(name="bikes")
 public class Bike 
 {
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
